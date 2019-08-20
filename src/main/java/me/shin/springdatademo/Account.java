bbada5c9c4d28df86af11bab38443724e4cf032e
aiden_shin <shin88@ejbt.co.kr>
2019-08-20 17:58:08 +0900

    엔티티 타입 맵핑

--- src/main/java/me/shin/springdatademo/Account.java
+++ src/main/java/me/shin/springdatademo/Account.java
@@ -1,8 +1,7 @@
 package me.shin.springdatademo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Account {
@@ -10,9 +9,18 @@ public class Account {
     @Id @GeneratedValue
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
     private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created = new Date();
+
+    private String yes;
+
+    @Transient
+    private String no;
 
     public Long getId() {
         return id;
