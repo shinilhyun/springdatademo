ae4fe3b9bc2e809281af0cb2009f8f58385cc88c
aiden_shin <shin88@ejbt.co.kr>
2019-08-20 13:04:45 +0900

    Initial commit

---
+++ src/main/java/me/shin/springdatademo/JpaRunner.java
@@ -0,0 +1,26 @@
+package me.shin.springdatademo;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Component
+@Transactional
+public class JpaRunner implements ApplicationRunner {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        Account account = new Account();
+        account.setUsername("ilhyun");
+        account.setPassword("jpa");
+        entityManager.persist(account);
+    }
+}
